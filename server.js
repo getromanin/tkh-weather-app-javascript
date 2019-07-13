4e96cc5e11e62032e96a6f4a2f490c20f70a8ab2
getromanin <romanh99@gmail.com>
2019-07-13 16:27:00 -0400

    set up post route and body-parser middleware

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('index');
+})
+
+app.post('/', function(req, res) {
+  res.render('index');
+  console.log(req.body.city);
 })
 
 app.listen(3000, function() {
