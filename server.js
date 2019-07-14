3a71f7428e72574e70a99677023664ba13b1d76a
getromanin <romanh99@gmail.com>
2019-07-14 01:36:43 -0400

    set up .env file and make api call

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+
+require('dotenv').config()
+
+// const API_KEY = process.env.weatherAPIKey;
+// const API_KEY = '3901c8ea6c8c3aa81af4848a5bc00fdb'
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -16,10 +22,19 @@ app.post('/', function(req, res) {
   console.log(req.body.city);
 
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?
-  q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.weatherAPIKey}`
 
-  res.render('index');
+  request(url, function (err, response, body) {
+    if(err) {
+      res.render('index', {
+        weather: null,
+        error: 'Error, please try again'
+      });
+    } else {
+      console.log('body:', body);
+    }
+  })
+
 })
 
 app.listen(3000, function() {
