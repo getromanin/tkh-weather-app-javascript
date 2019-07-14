dfbe6ec10b4efac42e3e82764791342fd68d69b7
getromanin <romanh99@gmail.com>
2019-07-14 16:34:09 -0400

    update HTML and server.js with specific comments

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+// pull in express module
 const express = require('express');
+// pull in bodyparser middleware to extract the body and expose on req.body
 const bodyParser = require('body-parser');
+// pull in the requst module
 const request = require('request');
+// create a new instance of express
 const app = express();
-
+// pull in the .env module to hide our api credentials
 require('dotenv').config()
-
+// allow access to our public static file
 app.use(express.static('public'));
+// allow access to our bodyParser
 app.use(bodyParser.urlencoded({ extended: true}));
-
+// set up EJS to be used
 app.set('view engine', 'ejs');
 
 // app.get works with the code below W/O using the locals object in the ejs file
@@ -22,31 +27,37 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
   res.render('index')
 })
-
+// respond to the POST request on the route
 app.post('/', function(req, res) {
   console.log(req.body)
   console.log(req.body.city);
-
+  // grab the city text from the form/req.body
   let city = req.body.city;
+  // creatre a URL string from the API
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.weatherAPIKey}`
-
+  // fetch the API using the request module
   request(url, function (err, response, body) {
+    // if the api doesnt work let the user know
     if(err) {
       res.render('index', {
         weather: null,
         error: 'Error, please try again'
       });
+      // otherwise move on to the API
     } else {
+      // store and parse through the JSON to make it readable
       let weather = JSON.parse(body);
-
+      // if we find an error i.e typo or empty call let the user know
       if(weather.main == undefined) {
         res.render('index', {
           weather: null,
           error: 'Error, please try again'
         });
+      // otherwise give the user the weather
       } else {
+        // create and store the string from the req.body
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-
+        // render the string to the view/EJS
         res.render('index', {
           weather: weatherText,
           error: null
@@ -54,7 +65,6 @@ app.post('/', function(req, res) {
       }
     }
   })
-
 })
 
 app.listen(3000, function() {
